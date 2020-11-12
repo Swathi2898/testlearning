0f1b617d89121c6e9ebd890d09b8b57c8ce154a0
Swathi2898 <swathialapati2808@gmail.com>
2020-11-12 12:38:17 +0530

    first commit

---
+++ interface class/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace interface_class
+{
+    interface IAccount
+    {
+        void Deposit();
+        void Withdraw();
+    }
+    public class Saving : IAccount
+    {
+        public void Deposit()
+        {
+            Console.WriteLine("Deposit to Saving Account");
+        }
+        public void Withdraw()
+        {
+            Console.WriteLine("Withdraw from Saving Account");
+        }
+    }
+    public class Current : IAccount
+    {
+        public void Deposit()
+        {
+            Console.WriteLine("Deposit to Current Account");
+        }
+        public void Withdraw()
+        {
+            Console.WriteLine("Withdraw from Current Account");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Saving s = new Saving();
+            s.Deposit();
+            s.Withdraw();
+
+            Current c = new Current();
+            c.Deposit();
+            c.Withdraw();
+        }
+    }
+}
